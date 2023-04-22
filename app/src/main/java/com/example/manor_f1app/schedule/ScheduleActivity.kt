2c30f5207c8e14553491ec876c0534756ae0eca2
Logan-Manor <lmmanor192@outlook.com>
2023-04-21 23:57:40 -0400

    Added Schedule

---
+++ app/src/main/java/com/example/manor_f1app/schedule/ScheduleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.manor_f1app.schedule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.manor_f1app.R
+
+class ScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule)
+    }
+}
