5cf91e932e5e5594f378c1317a88c82ce13c65a0
Logan Manor <lmmanor192@outlook.com>
2023-04-23 13:34:03 -0400

    Menu and homepage are working now onto design of the data

--- app/src/main/java/com/example/manor_f1app/MainActivity.kt
+++ app/src/main/java/com/example/manor_f1app/MainActivity.kt
@@ -2,29 +2,40 @@ package com.example.manor_f1app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.manor_f1app.databinding.ActivityMainBinding
+import com.example.manor_f1app.driver.DriverFragment
+import com.example.manor_f1app.schedule.ScheduleFragment
+import com.example.manor_f1app.team.TeamFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
-        menuInflater.inflate(R.menu.nav_menu,menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_schedule -> Toast.makeText(this, "schedule selected", Toast.LENGTH_LONG).show()
-            R.id.nav_driver -> Toast.makeText(this, "driver selected", Toast.LENGTH_LONG).show()
-            R.id.nav_team -> Toast.makeText(this, "team selected", Toast.LENGTH_LONG).show()
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.driver -> replaceFragment(DriverFragment())
+                R.id.team -> replaceFragment(TeamFragment())
+                R.id.schedule -> replaceFragment(ScheduleFragment())
+                else -> {
+                }
+            }
+            true
         }
+    }
 
-        return super.onOptionsItemSelected(item)
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+
     }
 }
